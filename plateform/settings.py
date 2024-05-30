770c5a80ff7e63b4dd82b0476477d7a4ad10f73c
MasterYoussef <149875654+MasterYoussef@users.noreply.github.com>
2024-05-30 20:30:49 +0100

    Update settings.py

--- plateform/settings.py
+++ plateform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3(ja0ka&j3x4i04gf0^-9=g@kp5gkel)*y9wms#^k015j^+s-@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
